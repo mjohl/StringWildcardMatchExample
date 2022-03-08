a4b29c190ec07fe366306562f88e13bd11366e1a
Michele Johl <Michele.Johl@brightstarr.com>
2022-03-08 05:40:00 +0000

    Initial commit

---
+++ StringWildcardMatchTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using Console = System.Console;
+using Microsoft.VisualBasic.CompilerServices;
+
+var ignoredDataDogErrors = new List<String>();
+
+ignoredDataDogErrors.Add("*No * files found*"); 
+ignoredDataDogErrors.Add("*No matching file found on SFTP server*");
+ignoredDataDogErrors.Add("*Unable to acquire access token for client id*");
+ignoredDataDogErrors.Add("*No such host is known*");
+ignoredDataDogErrors.Add("*Delta token not retrieved. A full sync will need to be performed*");
+
+if (ignoredDataDogErrors.Any(ignoredDataDogError =>
+        LikeOperator.LikeString("Error message was: 'No matching files found", ignoredDataDogError, Microsoft.VisualBasic.CompareMethod.Binary)))
+{
+        Console.WriteLine("Match Found!");
+}
